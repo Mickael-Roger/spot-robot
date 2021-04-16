e0f1f98a00a290dc0d189fee080ff76024838e55
Mickael Roger <mickael@mickael-roger.com>
2021-04-16 13:03:04 +0000

    Gamepad first part

---
+++ src/gamepad.py
@@ -0,0 +1,111 @@
+import evdev
+from posix_ipc import MessageQueue, O_CREX, unlink_message_queue
+
+from time import sleep, time
+
+INPUT_DEVICE="/dev/input/event0"
+
+MAX_MSG=5
+
+laydown_button='BTN_TOP'
+wakeup_button='BTN_THUMB'
+
+
+
+
+class SpotGamepad():
+
+    def __init__(self):
+        try:
+            unlink_message_queue("/spotmotion")
+            unlink_message_queue("/cameramotion")
+        except:
+            pass
+
+        self.motionqueue=MessageQueue('/spotmotion', flags=O_CREX, max_messages=MAX_MSG)
+        self.cameraqueue=MessageQueue('/cameramotion', flags=O_CREX, max_messages=MAX_MSG)
+
+        # Initialize Gamepad
+        self.gamepad=evdev.InputDevice(INPUT_DEVICE)
+        
+
+    def read_event(self):
+        for event in self.gamepad.read_loop():
+            if event.type == evdev.ecodes.EV_ABS:
+                absevent = evdev.categorize(event)
+
+                # If motion button
+                if evdev.ecodes.bytype[absevent.event.type][absevent.event.code] == 'ABS_HAT0Y':
+                    code = absevent.event.value
+
+                    if code == -1:
+                        self.send_msg('spotmotion', '{"action":"forward","time":"' + str(time() + '"}')
+                    elif code == 1:
+                        self.send_msg('spotmotion', '{"action":"backward","time":"' + str(time() + '"}')
+                    else:
+                        self.send_msg('spotmotion', '{"action":"stop","time":"' + str(time() + '"}')
+
+                if evdev.ecodes.bytype[absevent.event.type][absevent.event.code] == 'ABS_HAT0X':
+                    code = absevent.event.value
+
+                    if code == -1:
+                        self.send_msg('spotmotion', '{"action":"left","time":"' + str(time() + '"}')
+                    elif code == 1:
+                        self.send_msg('spotmotion', '{"action":"right","time":"' + str(time() + '"}')
+                    else:
+                        self.send_msg('spotmotion', '{"action":"stop","time":"' + str(time() + '"}')
+
+
+                # If body position joystick
+                if evdev.ecodes.bytype[absevent.event.type][absevent.event.code] == 'ABS_X':
+                    print('lateral movement: ' + str(absevent.event.value))
+
+                if evdev.ecodes.bytype[absevent.event.type][absevent.event.code] == 'ABS_Y':
+                    print('front movement: ' + str(absevent.event.value))
+
+
+                # If camera position joystick
+                if evdev.ecodes.bytype[absevent.event.type][absevent.event.code] == 'ABS_Z':
+                    print('lateral camera: ' + str(absevent.event.value))
+
+                if evdev.ecodes.bytype[absevent.event.type][absevent.event.code] == 'ABS_RZ':
+                    print('front camera: ' + str(absevent.event.value))
+
+
+            # If action button
+            if event.type == evdev.ecodes.EV_KEY:
+                keyevent = evdev.categorize(event)
+
+                # Laydown
+                if evdev.ecodes.bytype[keyevent.event.type][keyevent.event.code] == laydown_button:
+                    if keyevent.event.value == 0:
+                        print('laydown')
+
+                # Wake up
+                if evdev.ecodes.bytype[keyevent.event.type][keyevent.event.code] == wakeup_button:
+                    if keyevent.event.value == 0:
+                        print('wakeup')
+
+
+
+
+    def send_msg(self, queue, msg):
+
+        if self.cameraqueue.current_messages >= MAX_MSG:
+            self.cameraqueue.receive()
+
+        if self.motionqueue.current_messages >= MAX_MSG:
+            self.motionqueue.receive()
+
+        if queue == 'spotmotion':
+            self.motionqueue.send(msg)
+
+        elif queue == 'cameramotion':
+            self.cameraqueue.send(msg)
+
+
+if __name__ == "__main__":
+
+    gamepad=SpotGamepad()
+    gamepad.read_event()
+
--- src/spot.py
+++ src/spot.py
@@ -13,8 +13,37 @@ gyro_side = 0.
 gyro_time = 0.
 gyro_lock = threading.Lock()
 
+spot_forward=0
+spot_backward=0
+spot_left=0
+spot_right=0
+spot_bodyforward=0
+spot_bodybackward=0
+spot_bodyleft=0
+spot_bodyright=0
+spot_motiontime=0.
+
+
 def spotgyro():
     global gyro_front, gyro_side, gyro_time, gyro_time
+
+    mqgyro = MessageQueue("/gyro")
+
+    while all_run:
+        try:
+            msg = mqgyro.receive()
+            msgvalues=json.loads(msg[0])
+            gyro_lock.acquire()
+            gyro_front=float(msgvalues['front'])
+            gyro_side=float(msgvalues['side'])
+            gyro_time=float(msgvalues['time'])
+            gyro_lock.release()
+        except:
+            pass
+
+
+def spotmotion():
+    global time
 
     mqgyro = MessageQueue("/gyro")
 
